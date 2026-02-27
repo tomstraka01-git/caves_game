5a345404db2381ac688cc4aed7afffeb21775435
Your Name <tom.straka01@email.com>
2026-02-27 20:26:18 +0100

    added new character

--- scripts/Character.gd
+++ scripts/Character.gd
@@ -1,40 +1,46 @@
 extends CharacterBody2D
 
 @onready var anim_sprite = $Pivot/AnimatedSprite2D
-const SPEED = 300.0
-const JUMP_VELOCITY = -450.0
 
+const SPEED = 300.0
+const JUMP_VELOCITY = -300.0 
 
 func _physics_process(delta: float) -> void:
 	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	
+
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		anim_sprite.play("jump")
+
 
 	var direction = 0
-	
 	if Input.is_action_pressed("move_left"):
 		direction = -1
-		$Pivot.scale.x = direction
-		
+		$Pivot.scale.x = -1
 	elif Input.is_action_pressed("move_right"):
 		direction = 1
-		$Pivot.scale.x = direction
+		$Pivot.scale.x = 1
 
 	velocity.x = direction * SPEED
 
+
+	move_and_slide()
+
 	
+	var new_anim = ""
+
 	if not is_on_floor():
 		if velocity.y < 0:
-			anim_sprite.play("jump")
+			new_anim = "jump"  
 		else:
-			anim_sprite.play("idle")
+			new_anim = "fall"  
 	elif direction != 0:
-		anim_sprite.play("walk")
+		new_anim = "run"
 	else:
-		anim_sprite.play("idle")
-	move_and_slide()
+		new_anim = "idle"
+
+	
+	if anim_sprite.animation != new_anim:
+		anim_sprite.play(new_anim)
