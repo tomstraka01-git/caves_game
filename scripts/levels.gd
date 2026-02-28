bdb577ac75cb21a1835b624632209cc75718f11b
Your Name <tom.straka01@email.com>
2026-02-28 19:04:58 +0100

    added level 2

--- scripts/levels.gd
+++ scripts/levels.gd
@@ -49,4 +49,4 @@ func _on_level_0_pressed() -> void:
 func _on_level_1_pressed() -> void:
     get_tree().change_scene_to_file("res://scenes/level_1.tscn")
 func _on_level_2_pressed() -> void:
-    pass
+    get_tree().change_scene_to_file("res://scenes/level_2.tscn")
